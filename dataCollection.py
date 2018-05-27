3d83d496b9a4cdb840b011d8369cdc0d15519f7e
Xiaowei Huang <xiaowei@michaels-air.home>
2018-05-27 07:38:09 +0100

    add datacollection

---
+++ dataCollection.py
@@ -0,0 +1,111 @@
+#!/usr/bin/env python
+
+"""
+author: Xiaowei Huang
+"""
+
+import numpy as np
+import time
+import os
+import copy
+
+from configuration import directory_statistics_string
+
+class dataCollection:
+
+    index = 0
+    layer = 0
+    fileHandler = 0
+
+    def __init__(self,filenamePostfix):
+        self.runningTime = {}
+        self.manipulationPercentage = {}
+        self.l2Distance = {}
+        self.l1Distance = {}
+        self.l0Distance = {}
+        self.confidence = {}
+        self.fileName = "%s/dataCollection_%s.txt"%(directory_statistics_string,filenamePostfix)
+        self.fileHandler = open(self.fileName, 'w')
+        self.succPercent = 0
+        self.maxfeatures = 0
+        
+    def initialiseIndex(self, index):
+        self.index = index
+        
+    def addMaxFeatures(self, rt):
+        self.maxfeatures[self.index] = rt
+        
+    def addRunningTime(self, rt):
+        self.runningTime[self.index] = rt
+        
+    def addConfidence(self, cf):
+        self.confidence[self.index] = cf
+        
+    def addManipulationPercentage(self, mp):
+        self.manipulationPercentage[self.index] = mp
+        
+    def addl2Distance(self, eudist):
+        self.l2Distance[self.index] = eudist
+        
+    def addl1Distance(self, l1dist):
+        self.l1Distance[self.index] = l1dist
+        
+    def addl0Distance(self, l0dist):
+        self.l0Distance[self.index] = l0dist
+        
+    def addSuccPercent(self, sp):
+        self.succPercent = sp
+        
+    def addComment(self,str):
+        self.fileHandler.write(str)
+
+    def provideDetails(self): 
+        self.fileHandler.write("running time: \n")
+        for i,r in self.runningTime.items():
+            self.fileHandler.write("%s:%s\n"%(i,r))
+            
+        self.fileHandler.write("manipulation percentage: \n")
+        for i,r in self.manipulationPercentage.items():
+            self.fileHandler.write("%s:%s\n"%(i,r))
+            
+        self.fileHandler.write("L2 distance: \n")
+        for i,r in self.l2Distance.items():
+            self.fileHandler.write("%s:%s\n"%(i,r))
+            
+        self.fileHandler.write("L1 distance: \n")
+        for i,r in self.l1Distance.items():
+            self.fileHandler.write("%s:%s\n"%(i,r))
+            
+        self.fileHandler.write("L0 distance: \n")
+        for i,r in self.l0Distance.items():
+            self.fileHandler.write("%s:%s\n"%(i,r))
+            
+        self.fileHandler.write("confidence: \n")
+        for i,r in self.confidence.items():
+            self.fileHandler.write("%s:%s\n"%(i,r))
+        self.fileHandler.write("\n")
+        
+        self.fileHandler.write("max features: \n")
+        for i,r in self.maxfeatures.items():
+            self.fileHandler.write("%s:%s\n"%(i,r))
+        self.fileHandler.write("\n")
+            
+    def summarise(self):
+        if len(self.manipulationPercentage) == 0: 
+            self.fileHandler.write("none of the images were successfully manipulated. ")
+            return
+        else:
+            art = sum(self.runningTime.values()) / len(self.runningTime.values()) 
+            self.fileHandler.write("average running time: %s\n"%(art))
+            amp = sum(self.manipulationPercentage.values()) / len(self.manipulationPercentage.values())
+            self.fileHandler.write("average manipulation percentage: %s\n"%(amp))
+            eudist = sum(self.l2Distance.values()) / len(self.l2Distance.values())
+            self.fileHandler.write("average euclidean distance: %s\n"%(eudist))
+            l1dist = sum(self.l1Distance.values()) / len(self.l1Distance.values())
+            self.fileHandler.write("average L1 distance: %s\n"%(l1dist))
+            l0dist = sum(self.l0Distance.values()) / len(self.l0Distance.values())
+            self.fileHandler.write("average L0 distance: %s\n"%(l0dist))
+            self.fileHandler.write("success rate: %s\n"%(self.succPercent))
+        
+    def close(self):
+        self.fileHandler.close()
