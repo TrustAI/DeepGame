7ed5d988680f3eecfd5df773c2f18f765e634937
Min Wu <minwu-cs@users.noreply.github.com>
2018-06-05 20:55:38 +0100

    update CompetitiveAlphaBeta Class

--- CompetitiveAlphaBeta.py
+++ CompetitiveAlphaBeta.py
@@ -9,10 +9,83 @@ Author: Min Wu
 Email: min.wu@cs.ox.ac.uk
 """
 
+from FeatureExtraction import *
+from basics import *
+
 
 class CompetitiveAlphaBeta:
-    def __init__(self, image, model, eta, tau):
+    def __init__(self, image, model, eta, tau, bounds=(0, 1)):
         self.IMAGE = image
+        self.IMAGE_BOUNDS = bounds
         self.MODEL = model
-        self.ETA = eta
+        self.DIST_METRIC = eta[0]
+        self.DIST_VAL = eta[1]
         self.TAU = tau
+        self.LABEL, _ = self.MODEL.predict(self.IMAGE)
+
+        feature_extraction = FeatureExtraction(pattern='grey-box')
+        self.PARTITIONS = feature_extraction.get_partitions(self.IMAGE, self.MODEL, num_partition=10)
+
+        print("Distance metric %s, with bound value %s." % (self.DIST_METRIC, self.DIST_VAL))
+
+    def play_game(self, image):
+        self.player1(image)
+
+    def player1(self, image):
+        for partitionID in self.PARTITIONS.keys():
+            self.player2(image, partitionID)
+
+    def player2(self, image, partition_idx):
+        pixels = self.PARTITIONS[partition_idx]
+        self.target_pixels(image, pixels)
+
+    def target_pixels(self, image, pixels):
+        (row, col, chl) = image.shape
+
+        atomic_manipulations = []
+        manipulated_images = []
+        for (x, y) in pixels:
+            for z in range(chl):
+                atomic = (x, y, z, 1 * self.TAU)
+                valid, atomic_image = self.apply_atomic_manipulation(image, atomic)
+                if valid is True:
+                    manipulated_images.append(atomic_image)
+                    atomic_manipulations.append(atomic)
+                atomic = (x, y, z, -1 * self.TAU)
+                valid, atomic_image = self.apply_atomic_manipulation(image, atomic)
+                if valid is True:
+                    manipulated_images.append(atomic_image)
+                    atomic_manipulations.append(atomic)
+        manipulated_images = np.asarray(manipulated_images)
+
+        probabilities = self.MODEL.model.predict(manipulated_images)
+
+    def apply_atomic_manipulation(self, image, atomic):
+        atomic_image = image.copy()
+        chl = atomic[0:3]
+        manipulate = atomic[3]
+
+        if (atomic_image[chl] >= max(self.IMAGE_BOUNDS) and manipulate >= 0) or (
+                atomic_image[chl] <= min(self.IMAGE_BOUNDS) and manipulate <= 0):
+            valid = False
+            return valid, atomic_image
+        else:
+            if atomic_image[chl] + manipulate > max(self.IMAGE_BOUNDS):
+                atomic_image[chl] = max(self.IMAGE_BOUNDS)
+            elif atomic_image[chl] + manipulate < min(self.IMAGE_BOUNDS):
+                atomic_image[chl] = min(self.IMAGE_BOUNDS)
+            else:
+                atomic_image[chl] += manipulate
+            valid = True
+            return valid, atomic_image
+
+    def cal_distance(self, image1, image2):
+        if self.DIST_METRIC == 'L0':
+            return l0Distance(image1, image2)
+        elif self.DIST_METRIC == 'L1':
+            return l1Distance(image1, image2)
+        elif self.DIST_METRIC == 'L2':
+            return l2Distance(image1, image2)
+        else:
+            print("Unrecognised distance metric. "
+                  "Try 'L0', 'L1', or 'L2'.")
