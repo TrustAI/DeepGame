dd621ea6888421c746cf3139d5a4dd16d58c99e2
xiaoweih <xiaowei.huang@live.com>
2018-06-09 22:18:36 +0100

    update

--- CooperativeMCTS.py
+++ CooperativeMCTS.py
@@ -228,7 +228,7 @@ class MCTSCooperative:
 
     # start random sampling and return the Euclidean value as the value
     def sampling(self, index, availableActions):
-        nprint("start sampling node %s" % index)
+        print("start sampling node %s" % index)
         availableActions2 = copy.deepcopy(availableActions)
         sampleValues = []
         i = 0
