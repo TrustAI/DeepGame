1767b141bbab162f0333af44ae87de3998111371
Min Wu <minwu-cs@users.noreply.github.com>
2018-06-20 11:30:06 +0100

    NeuralNetwork Class: add GTSRB dataset

--- NeuralNetwork.py
+++ NeuralNetwork.py
@@ -13,9 +13,12 @@ from keras.datasets import mnist, cifar10
 from keras.models import Sequential, load_model
 from keras.layers import Dense, Dropout, Flatten, Conv2D, MaxPooling2D
 from keras.preprocessing.image import ImageDataGenerator
+from keras.preprocessing import image as Image
 from keras import backend as K
+from matplotlib import pyplot as plt
 
 from basics import assure_path_exists
+from DataSet import *
 
 
 # Define a Neural Network class.
@@ -160,6 +163,72 @@ class NeuralNetwork:
 
             self.model = model
 
+            # Train a gtsrb model.
+        elif self.data_set == 'gtsrb':
+            batch_size = 128
+            num_classes = 43
+            epochs = 50
+            img_rows, img_cols, img_chls = 48, 48, 3
+            data_augmentation = True
+
+            train = DataSet('gtsrb', 'training')
+            x_train, y_train = train.x, train.y
+            test = DataSet('gtsrb', 'test')
+            x_test, y_test = test.x, test.y
+            input_shape = (img_rows, img_cols, img_chls)
+
+            model = Sequential()
+            model.add(Conv2D(64, (3, 3), activation='relu', input_shape=input_shape))
+            model.add(Conv2D(64, (3, 3), activation='relu'))
+            model.add(MaxPooling2D(pool_size=(2, 2)))
+            model.add(Conv2D(128, (3, 3), activation='relu'))
+            model.add(Conv2D(128, (3, 3), activation='relu'))
+            model.add(MaxPooling2D(pool_size=(2, 2)))
+            model.add(Flatten())
+            model.add(Dense(256, activation='relu'))
+            model.add(Dropout(0.5))
+            model.add(Dense(256, activation='relu'))
+            model.add(Dense(num_classes, activation='softmax'))
+
+            opt = keras.optimizers.rmsprop(lr=0.0001, decay=1e-6)
+            model.compile(loss='categorical_crossentropy',
+                          optimizer=opt,
+                          metrics=['accuracy'])
+
+            if not data_augmentation:
+                print("Not using data augmentation.")
+                model.fit(x_train, y_train,
+                          batch_size=batch_size,
+                          epochs=epochs,
+                          validation_data=(x_test, y_test),
+                          shuffle=True)
+            else:
+                print("Using real-time data augmentation.")
+                datagen = ImageDataGenerator(
+                    featurewise_center=False,
+                    samplewise_center=False,
+                    featurewise_std_normalization=False,
+                    samplewise_std_normalization=False,
+                    zca_whitening=False,
+                    rotation_range=0,
+                    width_shift_range=0.1,
+                    height_shift_range=0.1,
+                    horizontal_flip=True,
+                    vertical_flip=False)
+
+                datagen.fit(x_train)
+                model.fit_generator(datagen.flow(x_train, y_train,
+                                                 batch_size=batch_size),
+                                    epochs=epochs,
+                                    validation_data=(x_test, y_test),
+                                    workers=4)
+
+            scores = model.evaluate(x_test, y_test, verbose=0)
+            print("Test loss:", scores[0])
+            print("Test accuracy:", scores[1])
+
+            self.model = model
+
         else:
             print("Unsupported dataset %s. Try 'mnist' or 'cifar10'." % self.data_set)
 
@@ -171,6 +240,10 @@ class NeuralNetwork:
 
         elif self.data_set == 'cifar10':
             self.model.save('models/cifar10.h5')
+            print("Neural network saved to disk.")
+
+        elif self.data_set == 'gtsrb':
+            self.model.save('models/gtsrb.h5')
             print("Neural network saved to disk.")
 
         else:
@@ -186,18 +259,44 @@ class NeuralNetwork:
             self.model = load_model('models/cifar10.h5')
             print("Neural network loaded from disk.")
 
+        elif self.data_set == 'gtsrb':
+            self.model = load_model('models/gtsrb.h5')
+            print("Neural network loaded from disk.")
+
         else:
             print("load_network: Unsupported dataset.")
 
     def save_input(self, image, filename):
-        image_cv = copy.deepcopy(image)
-        cv2.imwrite(filename, image_cv * 255.0, [cv2.IMWRITE_PNG_COMPRESSION, 9])
+        image = Image.array_to_img(image.copy())
+        plt.imsave(filename, image)
+        # causes discrepancy
+        # image_cv = copy.deepcopy(image)
+        # cv2.imwrite(filename, image_cv * 255.0, [cv2.IMWRITE_PNG_COMPRESSION, 9])
 
     def get_label(self, index):
         if self.data_set == 'mnist':
             labels = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
         elif self.data_set == 'cifar10':
             labels = ['airplane', 'automobile', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck']
+        elif self.data_set == 'gtsrb':
+            labels = ['speed limit 20 (prohibitory)', 'speed limit 30 (prohibitory)',
+                      'speed limit 50 (prohibitory)', 'speed limit 60 (prohibitory)',
+                      'speed limit 70 (prohibitory)', 'speed limit 80 (prohibitory)',
+                      'restriction ends 80 (other)', 'speed limit 100 (prohibitory)',
+                      'speed limit 120 (prohibitory)', 'no overtaking (prohibitory)',
+                      'no overtaking (trucks) (prohibitory)', 'priority at next intersection (danger)',
+                      'priority road (other)', 'give way (other)', 'stop (other)',
+                      'no traffic both ways (prohibitory)', 'no trucks (prohibitory)',
+                      'no entry (other)', 'danger (danger)', 'bend left (danger)',
+                      'bend right (danger)', 'bend (danger)', 'uneven road (danger)',
+                      'slippery road (danger)', 'road narrows (danger)', 'construction (danger)',
+                      'traffic signal (danger)', 'pedestrian crossing (danger)', 'school crossing (danger)',
+                      'cycles crossing (danger)', 'snow (danger)', 'animals (danger)',
+                      'restriction ends (other)', 'go right (mandatory)', 'go left (mandatory)',
+                      'go straight (mandatory)', 'go right or straight (mandatory)',
+                      'go left or straight (mandatory)', 'keep right (mandatory)',
+                      'keep left (mandatory)', 'roundabout (mandatory)',
+                      'restriction ends (overtaking) (other)', 'restriction ends (overtaking (trucks)) (other)']
         else:
             print("LABELS: Unsupported dataset.")
         return labels[index]
